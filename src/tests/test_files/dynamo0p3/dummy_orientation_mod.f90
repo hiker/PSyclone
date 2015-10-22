5297b9aec6df3ba04dd8694b39483ba595afa3bd
andrewporter <andrewporter@4bbb03d4-c1cc-11e1-9ca4-71d347b01581>
2015-10-22 09:48:50 +0000

    #361 PSyclone: add GungHo header to test fortran files

--- src/tests/test_files/dynamo0p3/dummy_orientation_mod.f90
+++ src/tests/test_files/dynamo0p3/dummy_orientation_mod.f90
@@ -1,3 +1,11 @@
+!-----------------------------------------------------------------------------
+! (c) The copyright relating to this work is owned jointly by the Crown,
+! Met Office and NERC 2015.
+! However, it has been created with the help of the GungHo Consortium,
+! whose members are identified at https://puma.nerc.ac.uk/trac/GungHo/wiki
+!-----------------------------------------------------------------------------
+! Author R. Ford STFC Daresbury Lab
+
 module dummy_orientation_mod
   type, extends(kernel_type) :: dummy_orientation_type
      type(arg_type), meta_args(4) =    &
