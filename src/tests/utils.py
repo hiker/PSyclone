6b648acef7f0a48c10f410d3515503147248f7c2
andrewporter <andrewporter@4bbb03d4-c1cc-11e1-9ca4-71d347b01581>
2015-10-23 08:27:21 +0000

    #119 PSyclone: pylint and pep8 fixes to utils.py

--- src/tests/utils.py
+++ src/tests/utils.py
@@ -1,12 +1,13 @@
-#-------------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 # (c) The copyright relating to this work is owned jointly by the Crown,
 # Met Office and NERC 2015.
 # However, it has been created with the help of the GungHo Consortium,
 # whose members are identified at https://puma.nerc.ac.uk/trac/GungHo/wiki
-#-------------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 # Author R. Ford STFC Daresbury Lab
 
 ''' test utilities '''
+
 
 def line_number(root, string_name):
     '''helper routine which returns the first index of the supplied
@@ -17,12 +18,13 @@ def line_number(root, string_name):
             return idx
     return -1
 
+
 def count_lines(root, string_name):
     '''helper routine which returns the number of lines that contain the
     supplied string'''
     count = 0
     lines = str(root).splitlines()
-    for curr_idx, line in enumerate(lines):
+    for line in lines:
         if string_name in line:
             count += 1
     return count
