680c5c93f67f7290282632553fd64fb7e26ad88a
andrewporter <andrewporter@4bbb03d4-c1cc-11e1-9ca4-71d347b01581>
2016-05-13 16:13:15 +0000

    #597 PSyclone: basic variable dependency working.

--- f2py_93/fparser/scripts/make_dag.py
+++ f2py_93/fparser/scripts/make_dag.py
@@ -9,6 +9,14 @@ except ImportError:
     from optparse import OptionParser
 from fparser.script_options import set_f2003_options
 
+def str_to_node_name(astring):
+    
+    new_string = astring.replace(" ","")
+    new_string = new_string.replace(",","_")
+    new_string = new_string.replace("+","p")
+    new_string = new_string.replace("-","m")
+    return new_string
+
 def walk(children, my_type):
     local_list = []
     for child in children:
@@ -17,6 +25,30 @@ def walk(children, my_type):
         try:
             local_list += walk(child.content, my_type)
         except AttributeError:
+            pass
+    return local_list
+
+def walk_items(children, my_type):
+    from fparser.Fortran2003 import Name, Section_Subscript_List
+    ignore_types = [Section_Subscript_List]
+    local_list = []
+    # children is a tuple
+    for idx, child in enumerate(children):
+        # Drop anything that is a child of any of our
+        # ignored types
+        if type(child) in ignore_types:
+            continue
+        if isinstance(child, Name):
+            suffix = ""
+            if idx < len(children)-1 and isinstance(children[idx+1],
+                                                   Section_Subscript_List):
+                # This is an array reference
+                suffix = "_" + str_to_node_name(str(children[idx+1]))
+            local_list.append(str(child)+suffix)
+        try:
+            local_list += walk_items(child.items, my_type)
+        except AttributeError as excinfo:
+            #print str(excinfo)
             pass
     return local_list
 
@@ -35,39 +67,29 @@ def runner (parser, options, args):
             from fparser.Fortran2003 import Module, Module_Subprogram_Part, \
                 Subroutine_Subprogram, Assignment_Stmt, Add_Operand, \
                 Level_2_Expr, Level_2_Unary_Expr, Real_Literal_Constant, \
-                Specification_Part
+                Specification_Part, Name
             subroutines = walk(program.content, Subroutine_Subprogram)
             for subroutine in subroutines:
                 print type(subroutine)
                 print dir(subroutine)
-                for item in subroutine.content:
-                    print type(item)
-                    if isinstance(item, Specification_Part):
-                        print dir(item)
+                #for item in subroutine.content:
+                #    print type(item)
+                #    if isinstance(item, Specification_Part):
+                #        print dir(item)
                 pluscount = 0
                 assignments = walk(subroutine.content, Assignment_Stmt)
                 for assign in assignments:
-                    print "--------------------------"
-                    print assign.item
-                    print assign.items[0], "<- {"  
-                    if isinstance(assign.items[2], Add_Operand):
-                        print dir(assign.items[2])
-                        print assign.items[2].use_names
-                        pluscount += 1
-                        print "plus{0}".format(pluscount)
-                        pass
-                    elif isinstance(assign.items[2], Level_2_Expr):
-                        print dir(assign.items[2])
-                        print assign.items[2]
-                        pass
-                    elif isinstance(assign.items[2], Level_2_Unary_Expr):
-                        pass
-                    elif isinstance(assign.items[2], Real_Literal_Constant):
-                        print assign.items[2]
-                        pass
-                    else:
-                        print "Unrecognised expression type: ", assign.item
-                        print "Type is ", type(assign.item)
+                    #print "--------------------------"
+                    #print assign.item
+
+                    var_list = walk_items(assign.items[1:], Name)
+
+                    assigned_to = walk_items([assign.items[0]], Name)
+                    subgraphstr = str(assigned_to[0]) + " -> {"
+                    for var in var_list:
+                        subgraphstr += " " + var
+                    subgraphstr += "}"
+                    print subgraphstr
 
         except Fortran2003.NoMatchError, msg:
             print 'parsing %r failed at %s' % (filename, reader.fifo_item[-1])
