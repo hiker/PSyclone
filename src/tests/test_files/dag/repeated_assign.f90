4b4d63b2a322b8cd101ed406e2116d75793c7470
andrewporter <andrewporter@4bbb03d4-c1cc-11e1-9ca4-71d347b01581>
2016-07-28 13:15:28 +0000

    #597 PSyclone: extend test case for repeated assignment

--- src/tests/test_files/dag/repeated_assign.f90
+++ src/tests/test_files/dag/repeated_assign.f90
@@ -3,7 +3,7 @@ module repeated_assign_mod
 
 contains
 
-  subroutine test_repeated_assign(aprod, var1, var2, var3)
+  subroutine test_repeated_assign1(aprod, var1, var2, var3)
     ! Simple routine that contains multiple updates to the
     ! same variable
     implicit none
@@ -14,6 +14,22 @@ contains
     aprod = aprod * var1
     aprod = aprod * aprod
 
-  end subroutine test_repeated_assign
+  end subroutine test_repeated_assign1
+
+  subroutine test_repeated_assign2(aprod, var1, var2, var3)
+    ! Simple routine that contains multiple updates to the
+    ! same variable
+    implicit none
+    real(wp), intent(inout) :: aprod
+    real(wp), intent(in) :: var1, var2, var3
+    ! Locals
+    real(wp) :: tmp1
+
+    tmp1 = 2.0*aprod
+    aprod = var1 * var2 * var3 * aprod
+    aprod = aprod * var1 * tmp1
+    aprod = aprod * aprod
+
+  end subroutine test_repeated_assign2
   
 end module repeated_assign_mod
